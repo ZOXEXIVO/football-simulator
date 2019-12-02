865f9365fb508a19493295be3043632c8f2bf5f5
ZOXEXIVO <zoxexivo@gmail.com>
2019-12-03 01:11:08 +0300

    update

--- src/simulator/src/core/context.rs
+++ src/simulator/src/core/context.rs
@@ -4,21 +4,24 @@ use chrono::{ Duration };
 
 #[derive(Clone)]
 pub struct SimulationContext {       
-    pub date: NaiveDateTime
+    pub date: NaiveDateTime,
+    pub hour: u32
 }
 
 impl SimulationContext {
       pub fn new(date: NaiveDateTime) -> Self {
             SimulationContext { 
-                  date
+                  date,
+                  hour: 0
             }
       }
 
       pub fn next_date(&mut self){
           self.date += Duration::hours(1);
+          self.hour = self.date.time().hour();
       }
 
       pub fn check_contract_expiration(&self) -> bool {
-          self.date.time().hour() == 0
+          self.hour == 0
       }  
 }
