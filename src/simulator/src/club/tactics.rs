82f64e2791c3a5509116a7b3528f97b1465f7dd4
Ivan Artemov <zoxexivo@gmail.com>
2019-12-31 21:05:26 +0300

    update

--- src/simulator/src/club/tactics.rs
+++ src/simulator/src/club/tactics.rs
@@ -1,17 +1,31 @@
 #[derive(Debug, Clone)]
-pub struct Tactics{
+pub struct Tactics {
     pub positioning: TacticsPositioning
 }
 
-impl Tactics{
-    pub fn new() -> Self{
-        Tactics{
+impl Tactics {
+    pub fn new() -> Self {
+        Tactics {
             positioning: TacticsPositioning::P442
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub enum TacticsPositioning{
-    P442
+pub enum TacticsPositioning {
+    T235,
+    T442,
+    T451,
+    T433,
+    T442Diamond,
+    T442DiamondWide,
+    T442Narrow,
+    T352,
+    T4231,
+    T4141,
+    T4411,
+    T343,
+    T1333,
+    T4312,
+    T4222,
 }
