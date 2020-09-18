813b1958b1eae8f4893f72549f9a5465406dd21a
ZOXEXIVO <zoxexivo@gmail.com>
2020-09-19 00:54:59 +0300

    fix ui

---
+++ src/ui/assets/mod.rs
@@ -0,0 +1,18 @@
+﻿use actix_web::web::ServiceConfig;
+use actix_web::{web, Result, HttpResponse};
+
+pub const FONT_CSS: &[u8] = include_bytes!("fonts.css");
+pub const STYLE_CSS: &[u8] = include_bytes!("styles.css");
+
+pub fn static_routes(cfg: &mut ServiceConfig) {
+    cfg.service(web::resource("/styles").route(web::get().to(serve_styles)));
+    cfg.service(web::resource("/fonts").route(web::get().to(serve_fonts)));
+}
+
+pub async fn serve_styles() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body(STYLE_CSS))
+}
+
+pub async fn serve_fonts() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body(FONT_CSS))
+}
---
+++ src/ui/countries/get/mod.rs
@@ -0,0 +1,47 @@
+﻿use actix_web::{web, HttpResponse, Result};
+use actix_web::web::Data;
+use crate::GameAppData;
+use askama::Template;
+use core::Country;
+use serde::{Deserialize};
+
+#[derive(Deserialize)]
+pub struct CountryGetRequest {
+    country_id: u32
+}
+
+#[derive(Template)]
+#[template(path = "countries/get/get.html")]
+pub struct CountryGetViewModel<'c> {
+    pub id: u32,
+    pub name: &'c str,
+    pub leagues: Vec<LeagueDto<'c>>
+}
+
+pub struct LeagueDto<'l> {
+    pub id: u32,
+    pub name: &'l str,
+}
+
+pub async fn country_get_action(state: Data<GameAppData>, route_params: web::Path<CountryGetRequest>) -> Result<HttpResponse> {
+    let guard = state.data.lock().unwrap();
+    
+    let mut simulator_data = guard.as_ref().unwrap();
+
+    let country: &Country = simulator_data.continents.iter().flat_map(|c| &c.countries)
+        .find(|country| country.id == route_params.country_id)
+        .unwrap();
+
+    let model = CountryGetViewModel {
+        id: country.id,
+        name: &country.name,
+        leagues: country.leagues.iter().map(|l| LeagueDto {
+            id: l.id,
+            name: &l.name,
+        }).collect()
+    };
+
+    let html = CountryGetViewModel::render(&model).unwrap();
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+}
