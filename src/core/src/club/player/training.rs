01956d9cc5f036eaa8af0f5903a239b80ebf9d53
ZOXEXIVO <zoxexivo@gmail.com>
2021-08-09 01:23:44 +0300

    fix training

--- src/core/src/club/player/training.rs
+++ src/core/src/club/player/training.rs
@@ -38,10 +38,10 @@ impl PlayerTraining {
     pub fn train(player: &mut Player, coach: &Staff) {
         match coach.behaviour.state {
             PersonBehaviourState::Good => {
-                player.skills.mental.train(IntegerUtils::random(-1, 1) as i8);
-                player.skills.technical.train(IntegerUtils::random(-1, 1) as i8)
+                player.skills.mental.train(IntegerUtils::random(-1, 2) as i8);
+                player.skills.technical.train(IntegerUtils::random(-1, 2) as i8)
             }
-            PersonBehaviourState::Normal => player.skills.train(IntegerUtils::random(-1, 1) as i8),
+            PersonBehaviourState::Normal => player.skills.train(IntegerUtils::random(-1, 2) as i8),
             PersonBehaviourState::Poor => player.skills.physical.train(IntegerUtils::random(-1, 1) as i8),
         }
     }
