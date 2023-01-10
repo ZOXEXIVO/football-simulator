1553cef8908a0d0341d91af3e3a5cf5fb7f8f713
Ivan Artemov (ZOXEXIVO) <zoxexivo@gmail.com>
2023-01-10 22:41:35 +0300

    pole positioning

--- src/core/src/lib.rs
+++ src/core/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate core;
 
 mod simulator;
 pub use simulator::*;
--- src/core/src/match/engine.rs
+++ src/core/src/match/engine.rs
@@ -1,4 +1,4 @@
-use crate::{Squad, SquadPlayer};
+use crate::{PlayerPosition, PositionType, Squad, SquadPlayer, POSITION_POSITIONING};
 
 pub struct FootballEngine<'s> {
     pub home_squad: Squad<'s>,
@@ -19,61 +19,51 @@ impl<'s> FootballEngine<'s> {
         };
 
         let mut field = Field {
-            width: 400,
-            height: 300,
-            ball: Ball::new(200, 150),
+            width: 150,
+            height: 100,
+            ball: Ball::new(150 / 2, 100 / 2),
             players: Vec::new(),
         };
 
+        setup_players(&mut field, &self.home_squad, &self.away_squad);
+
         match_details
     }
+}
 
-    fn setup_home_players(
-        home_squad: &Squad,
-        field: &mut Field,
-    ) -> Vec<(SquadPlayer<'s>, FieldPosition)> {
-        let mut players: Vec<(SquadPlayer<'s>, FieldPosition)> = Vec::new();
+fn setup_players<'s>(field: &'s mut Field<'s>, home_squad: &'s Squad, away_squad: &'s Squad) {
+    let mut players: Vec<(&SquadPlayer<'s>, FieldPosition)> = Vec::new();
 
-        // let field_width = field.width;
-        // let field_height = field.height;
-        //
-        // let defense_count = home_squad
-        //     .players
-        //     .iter()
-        //     .filter(|(_, pos)| *pos == FieldPosition::Defense)
-        //     .count();
-        // let midfield_count = home_squad
-        //     .players
-        //     .iter()
-        //     .filter(|(_, pos)| *pos == FieldPosition::Midfield)
-        //     .count();
-        // let attack_count = home_squad
-        //     .players
-        //     .iter()
-        //     .filter(|(_, pos)| *pos == FieldPosition::Attack)
-        //     .count();
-        //
-        // let y_step = field_height / (defense_count + midfield_count + attack_count) as u16;
-        // let mut current_y = 0;
-        //
-        // for (player, position) in home_squad.players.iter() {
-        //     match position {
-        //         FieldPosition::Defense => {
-        //             players.push((player, *position));
-        //             current_y += y_step;
-        //         }
-        //         FieldPosition::Midfield => {
-        //             players.push((player, *position));
-        //             current_y += y_step;
-        //         }
-        //         FieldPosition::Attack => {
-        //             players.push((player, *position));
-        //             current_y += y_step;
-        //         }
-        //     }
-        // }
+    // home
+    home_squad.main_squad.iter().for_each(|home_player| {
+        let player_positioning = POSITION_POSITIONING
+            .iter()
+            .find(|(positioning, _, _)| *positioning == home_player.position);
 
-        players
+        match player_positioning {
+            Some((_, PositionType::Home(x, y), _)) => {
+                players.push((home_player, FieldPosition::new(*x, *y)));
+            }
+            _ => panic!("Unknown home player position type"),
+        }
+    });
+
+    // away
+    away_squad.main_squad.iter().for_each(|away_player| {
+        let player_positioning = POSITION_POSITIONING
+            .iter()
+            .find(|(positioning, _, _)| *positioning == away_player.position);
+
+        match player_positioning {
+            Some((_, _, PositionType::Away(x, y))) => {
+                players.push((away_player, FieldPosition::new(*x, *y)));
+            }
+            _ => panic!("Unknown away player position type"),
+        }
+    });
+
+    for (player, position) in players {
+        field.players.push((player, position));
     }
 }
 
@@ -90,7 +80,7 @@ pub struct Field<'s> {
     pub width: u16,
     pub height: u16,
     pub ball: Ball,
-    pub players: Vec<(SquadPlayer<'s>, FieldPosition)>,
+    pub players: Vec<(&'s SquadPlayer<'s>, FieldPosition)>,
 }
 
 pub struct Ball {
@@ -108,6 +98,12 @@ impl Ball {
 pub struct FieldPosition {
     pub x: u16,
     pub y: u16,
+}
+
+impl FieldPosition {
+    pub fn new(x: u16, y: u16) -> Self {
+        FieldPosition { x, y }
+    }
 }
 
 pub enum MatchEvent {
