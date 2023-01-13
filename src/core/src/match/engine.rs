485d78cf4b7ed788683ac1c118e05471456709ed
Ivan Artemov (ZOXEXIVO) <zoxexivo@gmail.com>
2023-01-14 02:06:49 +0300

    engine refactoring

--- src/core/src/match/engine.rs
+++ src/core/src/match/engine.rs
@@ -14,32 +14,7 @@ impl<'s> FootballEngine<'s> {
     }
 
     pub fn play(&mut self) -> FootballMatchDetails {
-        let mut match_details = FootballMatchDetails {
-            score: Score { home: 0, away: 0 },
-            players_positions: Vec::new(),
-        };
-
-        let mut field = Field {
-            width: 150,
-            height: 100,
-            ball: Ball::new(150 / 2, 100 / 2),
-            players: Vec::new(),
-        };
-
-        for (player, position) in setup_players(&self.home_squad, &self.away_squad) {
-            field.players.push((player, position));
-        }
-
-        field.players.iter().for_each(|(player, position)| {
-            match_details.players_positions.push(PlayerPositionData {
-                player_id: player.player.id,
-                x: position.x,
-                y: position.y,
-                timestamp: 0,
-            });
-        });
-
-        match_details
+        Field::new(150, 100, &self.home_squad, &self.away_squad).play()
     }
 }
 
@@ -84,6 +59,15 @@ pub struct FootballMatchDetails {
     pub players_positions: Vec<PlayerPositionData>,
 }
 
+impl FootballMatchDetails {
+    pub fn new(score: Score, players_positions: Vec<PlayerPositionData>) -> Self {
+        FootballMatchDetails {
+            score,
+            players_positions,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct PlayerPositionData {
     pub player_id: u32,
@@ -103,6 +87,72 @@ pub struct Field<'s> {
     pub height: u16,
     pub ball: Ball,
     pub players: Vec<(&'s SquadPlayer<'s>, FieldPosition)>,
+}
+
+impl<'s> Field<'s> {
+    pub fn new(
+        width: u16,
+        height: u16,
+        home_squad: &'s Squad<'s>,
+        away_squad: &'s Squad<'s>,
+    ) -> Self {
+        let mut players_container =
+            Vec::with_capacity(home_squad.main_squad.len() + away_squad.main_squad.len());
+
+        for (player, position) in setup_players(&home_squad, &away_squad) {
+            players_container.push((player, position));
+        }
+
+        let mut field = Field {
+            width,
+            height,
+            ball: Ball::new(width / 2, height / 2),
+            players: players_container,
+        };
+
+        field
+    }
+
+    pub fn play(&mut self) -> FootballMatchDetails {
+        let mut match_details = FootballMatchDetails {
+            score: Score { home: 0, away: 0 },
+            players_positions: Vec::new(),
+        };
+
+        self.play_first_half(&mut match_details);
+
+        self.play_rest(&mut match_details);
+
+        self.play_second_half(&mut match_details);
+
+        match_details
+    }
+
+    fn play_first_half(&mut self, match_details: &mut FootballMatchDetails) {
+        let ms_step = 100;
+        let mut current_time = 0;
+
+        while current_time <= 45 * 60 * 1000 {
+            //self.ball.move_ball();
+
+            current_time += ms_step;
+        }
+    }
+
+    fn play_rest(&mut self, match_details: &mut FootballMatchDetails) {}
+
+    fn play_second_half(&mut self, match_details: &mut FootballMatchDetails) {}
+
+    pub fn write_positions(&self, match_details: &mut FootballMatchDetails) {
+        self.players.iter().for_each(|(player, position)| {
+            match_details.players_positions.push(PlayerPositionData {
+                player_id: player.player.id,
+                x: position.x,
+                y: position.y,
+                timestamp: 0,
+            });
+        });
+    }
 }
 
 pub struct Ball {
