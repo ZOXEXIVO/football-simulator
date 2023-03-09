6bd6b509d4c37be6c43cf4d10cd0d75f59f9d04e
zoxexivo <zoxexivo@gmail.com>
2023-03-09 22:20:50 +0300

    Fix field

--- src/core/src/match/engine/engine.rs
+++ src/core/src/match/engine/engine.rs
@@ -8,77 +8,72 @@ pub struct FootballEngine<const W: usize, const H: usize> {}
 
 impl<const W: usize, const H: usize> FootballEngine<W, H> {
     pub fn play(home_squad: TeamSquad, away_squad: TeamSquad) -> FootballMatchResult {
-        let mut context = MatchContext::with_field(MatchField::new(W, H));
+        let mut context = MatchContext::new();
 
-        context.field.setup(home_squad, away_squad);
+        let mut field = MatchField::new(W, H);
+
+        field.setup(home_squad, away_squad);
 
         context.result.write_team_players(
-            context.field.home_players.as_ref().unwrap(),
-            context.field.away_players.as_ref().unwrap(),
+            field.home_players.as_ref().unwrap(),
+            field.away_players.as_ref().unwrap(),
         );
 
         // First half
         context.state.set_game_state(GameState::FirstHalf);
-        Self::play_inner(&mut context);
+        Self::play_inner(&mut field, &mut context);
 
         {
-            context.field.swap_squads();
-            Self::play_rest_time(&mut context);
+            field.swap_squads();
+            Self::play_rest_time(&mut field);
         }
 
         // Second half
         context.state.set_game_state(GameState::SecondHalf);
-        Self::play_inner(&mut context);
+        Self::play_inner(&mut field, &mut context);
 
         if context.result.additinal_time_ms > 0 {
             // additional time
             context.state.set_game_state(GameState::ExtraTime);
 
-            Self::play_inner(&mut context);
+            Self::play_inner(&mut field, &mut context);
         }
 
         context.result
     }
 
-    fn play_rest_time(context: &mut MatchContext) {
-        context.field.players.iter_mut().for_each(|p| {
+    fn play_rest_time(field: &mut MatchField) {
+        field.players.iter_mut().for_each(|p| {
             p.player_attributes.rest(1000);
         })
     }
 
-    fn play_inner(context: &mut MatchContext) -> u64 {
+    fn play_inner(field: &mut MatchField, context: &mut MatchContext) -> u64 {
         let mut additional_time: u64 = 0;
 
         while context.time.time <= MATCH_HALF_TIME_MS {
-            let ball_update_events = context.field.ball.update(&context.state);
+            let ball_update_events = field.ball.update(&context.state);
 
             // handle ball
             Ball::handle_events(ball_update_events, context);
 
             let player_positions: Vec<FieldPosition> =
-                context.field.players.iter().map(|p| p.position).collect();
+                field.players.iter().map(|p| p.position).collect();
 
-            let player_update_events = context
-                .field
+            let player_update_events = field
                 .players
                 .iter_mut()
-                .flat_map(|p| {
-                    p.update(
-                        &context.state,
-                        &context.field.ball.position,
-                        &player_positions,
-                    )
+                .flat_map(|player| {
+                    player.update(&context.state, &field.ball.position, &player_positions)
                 })
                 .collect();
 
             // handle player
             MatchPlayer::handle_events(player_update_events, context);
 
-            context
-                .field
-                .write_match_positions(&mut context.result, context.time.time);
+            field.write_match_positions(&mut context.result, context.time.time);
 
-            context.time.time += MATCH_TIME_INCREMENT_MS;
+            context.increment_time();
         }
 
         additional_time
@@ -95,18 +90,20 @@ pub enum MatchEvent {
 pub struct MatchContext {
     pub state: MatchState,
     time: MatchTime,
-    field: MatchField,
     pub result: FootballMatchResult,
 }
 
 impl MatchContext {
-    pub fn with_field(field: MatchField) -> Self {
+    pub fn new() -> Self {
         MatchContext {
             state: MatchState::new(),
             time: MatchTime::new(),
-            field,
             result: FootballMatchResult::with_match_time(MATCH_HALF_TIME_MS),
         }
+    }
+
+    pub fn increment_time(&mut self) {
+        self.time.inc(MATCH_TIME_INCREMENT_MS);
     }
 }
 
@@ -120,5 +117,9 @@ pub struct MatchTime {
 impl MatchTime {
     pub fn new() -> Self {
         MatchTime { time: 0 }
+    }
+
+    pub fn inc(&mut self, val: u64) {
+        self.time += val;
     }
 }
