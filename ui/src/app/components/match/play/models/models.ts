198e9997fe9187d39139dbcaf6adb1a6fd694da9
Ivan Artemov (ZOXEXIVO) <zoxexivo@gmail.com>
2024-05-08 00:09:18 +0300

    update packages

--- ui/src/app/components/match/play/models/models.ts
+++ ui/src/app/components/match/play/models/models.ts
@@ -1,120 +1,119 @@
-﻿import {Sprite} from "@pixi/sprite";
-import {MatchLineupPlayerDto, ObjectPositionDto} from "../../services/match.api.service";
-import {Graphics} from "pixi.js";
+﻿import {ObjectPositionDto} from "../../services/match.api.service";
+import {Graphics, Sprite} from "pixi.js";
 
 export class MatchModel {
-  constructor() {
-    this.players = [];
-    this.ball = new BallModel([]);
-    this.squad = new SquadModel();
+    constructor() {
+        this.players = [];
+        this.ball = new BallModel([]);
+        this.squad = new SquadModel();
 
-    this.home_team = new TeamModel('', '');
-    this.away_team = new TeamModel('', '');
+        this.home_team = new TeamModel('', '');
+        this.away_team = new TeamModel('', '');
 
-    this.score = new ScoreModel(0, 0);
-  }
+        this.score = new ScoreModel(0, 0);
+    }
 
-  public players: PlayerModel[];
-  public ball: BallModel;
-  public squad: SquadModel;
+    public players: PlayerModel[];
+    public ball: BallModel;
+    public squad: SquadModel;
 
-  public home_team: TeamModel;
-  public away_team: TeamModel;
+    public home_team: TeamModel;
+    public away_team: TeamModel;
 
-  public score: ScoreModel
+    public score: ScoreModel
 }
 
 export class TeamModel {
-  constructor(name: string, slug: string) {
-    this.name = name;
-    this.slug = slug;
-  }
+    constructor(name: string, slug: string) {
+        this.name = name;
+        this.slug = slug;
+    }
 
-  public name: string;
-  public slug: string;
+    public name: string;
+    public slug: string;
 }
 
 
 export class ScoreModel {
-  constructor(home_goals: number, away_goals: number) {
-    this.home_goals = home_goals;
-    this.away_goals = away_goals;
-  }
+    constructor(home_goals: number, away_goals: number) {
+        this.home_goals = home_goals;
+        this.away_goals = away_goals;
+    }
 
-  public home_goals: number;
-  public away_goals: number;
+    public home_goals: number;
+    public away_goals: number;
 }
 
 export class PlayerModel {
-  constructor(id: number, idHome: boolean, data: ObjectPositionDto[]) {
-    this.id = id;
-    this.isHome = idHome;
-    this.obj = null;
-    this.currentCoordIdx = 0;
-    this.data = data;
-  }
+    constructor(id: number, idHome: boolean, data: ObjectPositionDto[]) {
+        this.id = id;
+        this.isHome = idHome;
+        this.obj = null;
+        this.currentCoordIdx = 0;
+        this.data = data;
+    }
 
-  public id: number;
-  public isHome: boolean;
-  public obj: Graphics | null;
-  public currentCoordIdx: number;
-  public data: ObjectPositionDto[];
+    public id: number;
+    public isHome: boolean;
+    public obj: Graphics | null;
+    public currentCoordIdx: number;
+    public data: ObjectPositionDto[];
 }
 
 export class BallModel {
-  constructor(data: ObjectPositionDto[]) {
-    this.obj = null;
-    this.currentCoordIdx = 0;
-    this.data = data;
-  }
+    constructor(data: ObjectPositionDto[]) {
+        this.obj = null;
+        this.currentCoordIdx = 0;
+        this.data = data;
+    }
 
-  public obj?: Sprite | null;
-  public currentCoordIdx: number;
-  public data: ObjectPositionDto[];
+    public obj?: Sprite | null;
+    public currentCoordIdx: number;
+    public data: ObjectPositionDto[];
 }
 
 // Squad
 
 export class SquadModel {
-  constructor() {
-    this.home = [];
-    this.home_subs = [];
+    constructor() {
+        this.home = [];
+        this.home_subs = [];
 
-    this.away = [];
-    this.away_subs = [];
-  }
+        this.away = [];
+        this.away_subs = [];
+    }
 
-  public home: SquadPlayerModel[];
-  public home_subs: SquadPlayerModel[];
+    public home: SquadPlayerModel[];
+    public home_subs: SquadPlayerModel[];
 
-  public away: SquadPlayerModel[];
-  public away_subs: SquadPlayerModel[];
+    public away: SquadPlayerModel[];
+    public away_subs: SquadPlayerModel[];
 }
 
 export class SquadPlayerModel {
-  constructor(id: number,
-              first_name: string, last_name: string, middle_name: string,
-              position: string, team_slug: string) {
-    this.id = id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.middle_name = middle_name;
-    this.position = position;
-    this.team_slug = team_slug;
-  }
+    constructor(id: number,
+                first_name: string, last_name: string, middle_name: string,
+                position: string, team_slug: string) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.middle_name = middle_name;
+        this.position = position;
+        this.team_slug = team_slug;
+    }
 
-  public id: number;
-  public first_name: string;
-  public last_name: string;
-  public middle_name: string;
-  public position: string;
-  public team_slug: string;
+    public id: number;
+    public first_name: string;
+    public last_name: string;
+    public middle_name: string;
+    public position: string;
+    public team_slug: string;
 }
 
 export class MatchLineupSetupCompleted {
-  constructor(matchTimeMs: number) {
-    this.matchTimeMs = matchTimeMs;
-  }
+    constructor(matchTimeMs: number) {
+        this.matchTimeMs = matchTimeMs;
+    }
 
-  public matchTimeMs: number;
+    public matchTimeMs: number;
 }
--- ui/src/app/components/match/play/play.match.component.ts
+++ ui/src/app/components/match/play/play.match.component.ts
@@ -1,187 +1,186 @@
 ﻿import {
-  AfterViewInit, ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Input,
-  NgZone,
-  OnDestroy,
-  ViewChild
+    AfterViewInit, ChangeDetectorRef,
+    Component,
+    ElementRef,
+    Input,
+    NgZone,
+    OnDestroy,
+    ViewChild
 } from '@angular/core';
 import * as PIXI from 'pixi.js';
-import {Sprite} from '@pixi/sprite';
-import {Graphics} from "pixi.js";
+import {Graphics, Sprite} from "pixi.js";
 import {MatchDataService} from "../services/match.data.service";
 import {POLE_COORDS} from "./models/constants";
 import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
 
 @UntilDestroy()
 @Component({
-  selector: 'play-match',
-  template: '<div #matchContainer style="min-height: 500px;"></div>'
+    selector: 'play-match',
+    template: '<div #matchContainer style="min-height: 500px;"></div>'
 })
 export class MatchPlayComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('matchContainer') matchContainer!: ElementRef;
+    @ViewChild('matchContainer') matchContainer!: ElementRef;
 
-  application: PIXI.Application | null = null;
+    application: PIXI.Application | null = null;
 
-  isDisposed = false;
+    isDisposed = false;
 
-  @Input()
-  currentTime = 0;
+    @Input()
+    currentTime = 0;
 
-  constructor(private zone: NgZone,
-              private matchDataService: MatchDataService,
-              private changeDetectorRef: ChangeDetectorRef) {
-    console.log(PIXI.VERSION);
-  }
-
-  public ngAfterViewInit(): void {
-
-    this.initGraphics();
-  }
-
-  initGraphics() {
-    this.zone.runOutsideAngular(
-      (): void => {
-        this.application = new PIXI.Application({
-          antialias: true,
-          //resizeTo: this.matchContainer.nativeElement
-        });
-
-        this.matchContainer.nativeElement.appendChild(this.application.view);
-
-        this.application.stage.addChild(this.createBackground(this.application));
-
-        const ball = this.createBall();
-        this.matchDataService.matchData.ball.obj = ball;
-        this.application.stage.addChild(ball);
-
-        this.matchDataService.matchData.players.forEach(player => {
-          let translatedCoords = this.translateToField(player.data[0].x, player.data[0].y);
-          const playerObj = this.createPlayer(translatedCoords.x, translatedCoords.y, player.isHome)
-
-          player.obj = playerObj;
-
-          this.application?.stage.addChild(playerObj);
-        });
-
-        // console.log('players count = ' + this.matchDataService.matchData.players.length);
-
-        // DEBUG
-        // this.application.stage.addChild(this.createPlayer(POLE_COORDS.tl.x, POLE_COORDS.tl.y));
-        // this.application.stage.addChild(this.createPlayer(POLE_COORDS.tr.x, POLE_COORDS.tr.y));
-        // this.application.stage.addChild(this.createPlayer(POLE_COORDS.bl.x, POLE_COORDS.bl.y));
-        // this.application.stage.addChild(this.createPlayer(POLE_COORDS.br.x, POLE_COORDS.br.y));
-
-        this.application.ticker.add((delta) => {
-          if(this.isDisposed){
-            return;
-          }
-
-          // console.log('time=' + this.currentTime);
-
-          this.matchDataService.getData(this.currentTime).pipe(untilDestroyed(this)).subscribe(data => {
-            const ballObject = this.matchDataService.matchData.ball.obj!;
-
-            let coord = this.translateToField(data.ball.x, data.ball.y);
-
-            //if(ballObject.x != coord.x && ballObject.y != coord.y){
-              //console.log(`ball move x = ${ballObject.x}, y = ${ballObject.y}`);
-
-              ballObject.x = coord.x;
-              ballObject.y = coord.y;
-
-              const scaleFactor= (data.ball.z + 20) / 20;
-
-              ballObject.scale.set(scaleFactor, scaleFactor);
-            //}
-
-            this.matchDataService.matchData.players.forEach((player, index) => {
-              const playerObject = player.obj!;
-              const playerData = data.players.find(p => p.playerId == player.id);
-
-              if(playerData && playerData.position){
-
-                const playerPosition = playerData.position;
-
-                if(index == 0){
-                  //console.log(`time = ${this.currentTime}: player position = (${playerPosition.x}, ${playerPosition.y})`);
-                }
-
-                if(playerPosition){
-                  let playerTranslatedPositions = this.translateToField(
-                    playerPosition.x,
-                    playerPosition.y
-                  );
-
-                  playerObject.x = playerTranslatedPositions.x;
-                  playerObject.y = playerTranslatedPositions.y;
-
-                  const scaleFactor= (playerPosition.z + 20) / 20;
-
-                  playerObject.scale.set(scaleFactor, scaleFactor);
-                }
-              }
-            });
-          });
-        });
-
-        this.application.render();
-        //this.changeDetectorRef.detectChanges();
-      }
-    );
-  }
-
-  translateToField(x: number, y: number) {
-    let scaleX = (POLE_COORDS.tr.x - POLE_COORDS.tl.x) / 840;
-    let scaleY = (POLE_COORDS.bl.y - POLE_COORDS.tl.y) / 545;
-
-    return {
-      x: POLE_COORDS.tl.x + (x * scaleX),
-      y: POLE_COORDS.tl.y + (y * scaleY)
+    constructor(private zone: NgZone,
+                private matchDataService: MatchDataService,
+                private changeDetectorRef: ChangeDetectorRef) {
+        console.log(PIXI.VERSION);
     }
-  }
 
-  createPlayer(x: number, y: number, isHome: boolean) : Graphics {
-    const homeColor = 0x0000ff;
-    const awayColor = 0xff0000;
+    public ngAfterViewInit(): void {
 
-    const circle: Graphics = new PIXI.Graphics();
+        this.initGraphics();
+    }
 
-    circle.beginFill(isHome? homeColor : awayColor);
-    circle.drawCircle(x, y, 6);
-    circle.endFill();
+    initGraphics() {
+        this.zone.runOutsideAngular(
+            (): void => {
+                this.application = new PIXI.Application({
+                    antialias: true,
+                    //resizeTo: this.matchContainer.nativeElement
+                });
 
-    return circle;
-  }
+                this.matchContainer.nativeElement.appendChild(this.application.view);
 
-  createBackground(app: PIXI.Application) {
-    // Background
-    const landscapeTexture = PIXI.Texture.from('assets/images/match/field.svg');
+                this.application.stage.addChild(this.createBackground(this.application));
 
-    const background = new PIXI.Sprite(landscapeTexture);
-    background.width = app.screen.width;
-    background.height = app.screen.height;
+                const ball = this.createBall();
+                this.matchDataService.matchData.ball.obj = ball;
+                this.application.stage.addChild(ball);
 
-    return background;
-  }
+                this.matchDataService.matchData.players.forEach(player => {
+                    let translatedCoords = this.translateToField(player.data[0].x, player.data[0].y);
+                    const playerObj = this.createPlayer(translatedCoords.x, translatedCoords.y, player.isHome)
 
-  createBall(): Sprite {
-    let center_x = POLE_COORDS.tl.x + ((POLE_COORDS.tr.x - POLE_COORDS.tl.x) / 2);
-    let center_y = POLE_COORDS.tl.y + ((POLE_COORDS.bl.y - POLE_COORDS.tl.y) / 2);
+                    player.obj = playerObj;
 
-    const ball: PIXI.Sprite = PIXI.Sprite.from(
-      'assets/images/match/ball.png'
-    );
+                    this.application?.stage.addChild(playerObj);
+                });
 
-    ball.x = center_x;
-    ball.y = center_y;
+                // console.log('players count = ' + this.matchDataService.matchData.players.length);
 
-    return ball;
-  }
+                // DEBUG
+                // this.application.stage.addChild(this.createPlayer(POLE_COORDS.tl.x, POLE_COORDS.tl.y));
+                // this.application.stage.addChild(this.createPlayer(POLE_COORDS.tr.x, POLE_COORDS.tr.y));
+                // this.application.stage.addChild(this.createPlayer(POLE_COORDS.bl.x, POLE_COORDS.bl.y));
+                // this.application.stage.addChild(this.createPlayer(POLE_COORDS.br.x, POLE_COORDS.br.y));
 
-  ngOnDestroy(): void {
-    this.isDisposed = true;
-    this.application?.ticker.stop();
-  }
+                this.application.ticker.add((delta) => {
+                    if (this.isDisposed) {
+                        return;
+                    }
+
+                    // console.log('time=' + this.currentTime);
+
+                    this.matchDataService.getData(this.currentTime).pipe(untilDestroyed(this)).subscribe(data => {
+                        const ballObject = this.matchDataService.matchData.ball.obj!;
+
+                        let coord = this.translateToField(data.ball.x, data.ball.y);
+
+                        //if(ballObject.x != coord.x && ballObject.y != coord.y){
+                        //console.log(`ball move x = ${ballObject.x}, y = ${ballObject.y}`);
+
+                        ballObject.x = coord.x;
+                        ballObject.y = coord.y;
+
+                        const scaleFactor = (data.ball.z + 20) / 20;
+
+                        ballObject.scale.set(scaleFactor, scaleFactor);
+                        //}
+
+                        this.matchDataService.matchData.players.forEach((player, index) => {
+                            const playerObject = player.obj!;
+                            const playerData = data.players.find(p => p.playerId == player.id);
+
+                            if (playerData && playerData.position) {
+
+                                const playerPosition = playerData.position;
+
+                                if (index == 0) {
+                                    //console.log(`time = ${this.currentTime}: player position = (${playerPosition.x}, ${playerPosition.y})`);
+                                }
+
+                                if (playerPosition) {
+                                    let playerTranslatedPositions = this.translateToField(
+                                        playerPosition.x,
+                                        playerPosition.y
+                                    );
+
+                                    playerObject.x = playerTranslatedPositions.x;
+                                    playerObject.y = playerTranslatedPositions.y;
+
+                                    const scaleFactor = (playerPosition.z + 20) / 20;
+
+                                    playerObject.scale.set(scaleFactor, scaleFactor);
+                                }
+                            }
+                        });
+                    });
+                });
+
+                this.application.render();
+                //this.changeDetectorRef.detectChanges();
+            }
+        );
+    }
+
+    translateToField(x: number, y: number) {
+        let scaleX = (POLE_COORDS.tr.x - POLE_COORDS.tl.x) / 840;
+        let scaleY = (POLE_COORDS.bl.y - POLE_COORDS.tl.y) / 545;
+
+        return {
+            x: POLE_COORDS.tl.x + (x * scaleX),
+            y: POLE_COORDS.tl.y + (y * scaleY)
+        }
+    }
+
+    createPlayer(x: number, y: number, isHome: boolean): Graphics {
+        const homeColor = 0x0000ff;
+        const awayColor = 0xff0000;
+
+        const circle: Graphics = new PIXI.Graphics();
+
+        circle.beginFill(isHome ? homeColor : awayColor);
+        circle.drawCircle(x, y, 6);
+        circle.endFill();
+
+        return circle;
+    }
+
+    createBackground(app: PIXI.Application) {
+        // Background
+        const landscapeTexture = PIXI.Texture.from('assets/images/match/field.svg');
+
+        const background = new PIXI.Sprite(landscapeTexture);
+        background.width = app.screen.width;
+        background.height = app.screen.height;
+
+        return background;
+    }
+
+    createBall(): Sprite {
+        let center_x = POLE_COORDS.tl.x + ((POLE_COORDS.tr.x - POLE_COORDS.tl.x) / 2);
+        let center_y = POLE_COORDS.tl.y + ((POLE_COORDS.bl.y - POLE_COORDS.tl.y) / 2);
+
+        const ball: PIXI.Sprite = PIXI.Sprite.from(
+            'assets/images/match/ball.png'
+        );
+
+        ball.x = center_x;
+        ball.y = center_y;
+
+        return ball;
+    }
+
+    ngOnDestroy(): void {
+        this.isDisposed = true;
+        this.application?.ticker.stop();
+    }
 }
